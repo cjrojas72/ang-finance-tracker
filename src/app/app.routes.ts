4e87022628f70703ff13540fa6a75c350adbcddc
cjrojas72 <cjrojas72@gmail.com>
2025-01-23 19:11:20 -0500

    set up routes and add tailwind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { IncomeComponent } from './views/income/income.component';
+import { ExpensesComponent } from './views/expenses/expenses.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent},
     { path:'home', component: HomeComponent },
+    { path: 'income', component: IncomeComponent},
+    { path: 'expenses', component: ExpensesComponent}
 ];
--- src/app/layout/menu/menu.component.ts
+++ src/app/layout/menu/menu.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import {MatListModule} from '@angular/material/list';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router'
 
 
 @Component({
   selector: 'app-menu',
-  imports: [MatListModule],
+  imports: [MatListModule, CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css',
   standalone: true,
 })
 export class MenuComponent {
-
 }
